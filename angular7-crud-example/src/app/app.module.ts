35f3092f28cba662a6412c2dd53c6d09822cf386
moudgalya1223 <157271461+moudgalya1223@users.noreply.github.com>
2024-11-18 04:45:51 +0000

    upgraded to 18

--- angular7-crud-example/src/app/app.module.ts
+++ angular7-crud-example/src/app/app.module.ts
@@ -7,28 +7,21 @@ import { AddUserComponent } from './add-user/add-user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { ListUserComponent } from './list-user/list-user.component';
 import {ApiService} from "./core/api.service";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import {ReactiveFormsModule} from "@angular/forms";
 import {routing} from "./app.routing";
 import {TokenInterceptor} from "./core/interceptor";
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    AddUserComponent,
-    EditUserComponent,
-    ListUserComponent
-  ],
-  imports: [
-    BrowserModule,
-    routing,
-    ReactiveFormsModule,
-    HttpClientModule
-  ],
-  providers: [ApiService, {provide: HTTP_INTERCEPTORS,
-    useClass: TokenInterceptor,
-    multi : true}],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        LoginComponent,
+        AddUserComponent,
+        EditUserComponent,
+        ListUserComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        routing,
+        ReactiveFormsModule], providers: [ApiService, { provide: HTTP_INTERCEPTORS,
+            useClass: TokenInterceptor,
+            multi: true }, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
